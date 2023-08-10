576417c0c11ae6724934221a541fbda306562c8a
zearkiatos <caprilespe@outlook.com>
2023-08-10 00:01:05 -0400

    [WIP] Implementing classes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
---
+++ flaskr/models/song.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
