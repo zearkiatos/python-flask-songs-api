4dc95fd3c2f10ad28c212a0cb22e4d4f79e092a1
zearkiatos <caprilespe@outlook.com>
2023-08-09 23:41:58 -0400

    Basic structure

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+activate () {
+    if [ -d "venv" ] 
+    then
+        echo "Python 🐍 environment was activated"
+        source venv/bin/activate
+    else
+        echo "The folder environment doesn't exist"
+        python3 -m venv venv
+        source venv/bin/activate
+        echo "The environment folder was created and the python 🐍 environment was activated"
+    fi
+}
+
+install () {
+    pip install -r requirements.txt
+}
+
+run () {
+    cd flaskr
+    if [ -z "$1" ]
+    then
+        flask run
+    else
+        flask run -p $1
+    fi
+}
