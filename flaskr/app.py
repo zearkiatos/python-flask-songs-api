58ea6883edc54d81216d17f1c7c97f48e9d1361a
zearkiatos <caprilespe@outlook.com>
2023-08-25 20:42:17 -0400

    Add cors configuration

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
 from flaskr import create_app
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from .dataContext.sqlAlchemyContext import db
 from .views import SongsView, SongView, SignInView, LogInView, AlbumsView, AlbumView, UserView, AlbumsUserView, SongsAlbumView
 
@@ -10,6 +11,8 @@ app_context.push()
 
 db.init_app(app)
 db.create_all()
+
+cors = CORS(app)
 
 api = Api(app)
 api.add_resource(SongsView, '/songs')
